1658aec0bc1d86f649037da60a959fbdafb4d8a0
lyhou123 <lyhou282@gmail.com>
2024-05-14 18:09:17 +0700

    i have finish file upload

---
+++ src/main/java/org/project/user/feature/file/FileService.java
@@ -0,0 +1,17 @@
+package org.project.user.feature.file;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.project.user.feature.file.dto.FileResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface FileService {
+    FileResponse uploadSingleFile(MultipartFile file, HttpServletRequest request);
+    List<String> uploadMultipleFiles(MultipartFile[] files);
+
+    ResponseEntity<Resource> serveFile(String filename, HttpServletRequest request);
+    void deleteFile(String filename);
+}
---
+++ src/main/java/org/project/user/feature/file/dto/FileResponse.java
@@ -0,0 +1,13 @@
+package org.project.user.feature.file.dto;
+
+import lombok.Builder;
+
+@Builder
+public record FileResponse(
+        String filename,
+        String fullUrl,
+        String downloadUrl,
+        String fileType,
+        float size
+) {
+}
